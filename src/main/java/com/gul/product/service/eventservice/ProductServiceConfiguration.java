1b13b4f221dfddded1e129833999d4910b57d2df
Usman <azhar.rao@gmail.com>
2015-09-09 00:27:39 -0700

    update database configuration for Heroku

--- src/main/java/com/gul/product/service/eventservice/ProductServiceConfiguration.java
+++ src/main/java/com/gul/product/service/eventservice/ProductServiceConfiguration.java
@@ -46,8 +46,10 @@ public class ProductServiceConfiguration extends Configuration {
 
     @JsonProperty("database")
 	public DataSourceFactory getDatabase() {
+        LOGGER.info("Dropwizard dummy DB URL (will be overridden)=" + database.getUrl());
         DatabaseConfiguration databaseConfiguration = HerokuDatabaseConfiguration.create(System.getenv("DATABASE_URL"));
         database = databaseConfiguration.getDataSourceFactory(null);
+        LOGGER.info("Heroku DB URL=" + database.getUrl());
         return database;
 	}
 

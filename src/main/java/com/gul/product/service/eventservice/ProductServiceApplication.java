a36e726900724c39656e9dc65599223aaa3f6c49
Usman <azhar.rao@gmail.com>
2015-09-10 14:37:58 -0700

    connecting to heroku database

--- src/main/java/com/gul/product/service/eventservice/ProductServiceApplication.java
+++ src/main/java/com/gul/product/service/eventservice/ProductServiceApplication.java
@@ -10,14 +10,21 @@ import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+
 import java.util.Map;
+
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gul.product.service.cli.RenderCommand;
 import com.gul.product.service.core.Template;
 import com.gul.product.service.persistance.ProductDao;
 import com.gul.product.service.representation.Product;
 import com.gul.product.service.resources.ProductResource;
 
 public class ProductServiceApplication extends Application<ProductServiceConfiguration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductServiceConfiguration.class);
 	
     public static void main(String[] args) throws Exception {
         new ProductServiceApplication().run(args);
@@ -37,10 +44,8 @@ public class ProductServiceApplication extends Application<ProductServiceConfigu
             
 	@Override
 	public void initialize(Bootstrap<ProductServiceConfiguration> bootstrap) {
-		bootstrap.setConfigurationSourceProvider(new SubstitutingSourceProvider(
-						bootstrap.getConfigurationSourceProvider(),
-						new EnvironmentVariableSubstitutor(false)));
-//		bootstrap.addCommand(new RenderCommand());	// custom class
+        LOGGER.info("Initializing configuration");
+		bootstrap.addCommand(new RenderCommand());	// custom class
 		bootstrap.addBundle(new AssetsBundle());
         bootstrap.addBundle(new MigrationsBundle<ProductServiceConfiguration>() {
             @Override
@@ -49,16 +54,12 @@ public class ProductServiceApplication extends Application<ProductServiceConfigu
             }
         });
 		bootstrap.addBundle(hibernateBundle);
-        bootstrap.addBundle(new ViewBundle<ProductServiceConfiguration>() {
-            @Override
-            public Map<String, Map<String, String>> getViewConfiguration(ProductServiceConfiguration configuration) {
-                return configuration.getViewRendererConfiguration();
-            }
-        });
+        bootstrap.addBundle(new ViewBundle());
 	}
 	
 	@Override
 	public void run(ProductServiceConfiguration configuration, Environment environment) throws Exception {
+        LOGGER.info("Starting the Product data service");
         final ProductDao dao = new ProductDao(hibernateBundle.getSessionFactory());
         final Template template = configuration.buildTemplate();
         environment.jersey().register(RolesAllowedDynamicFeature.class);

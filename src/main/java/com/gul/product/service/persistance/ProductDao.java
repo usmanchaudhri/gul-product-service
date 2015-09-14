017437cdc23c155e67b820b7806fbf2d30abaea2
Usman <azhar.rao@gmail.com>
2015-09-14 00:33:17 -0700

    running after manually creating the DB

--- src/main/java/com/gul/product/service/persistance/ProductDao.java
+++ src/main/java/com/gul/product/service/persistance/ProductDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.hibernate.SessionFactory;
 import com.gul.product.service.representation.Product;
 
+// dao class
 public class ProductDao extends AbstractDAO<Product> {
 
 	public ProductDao(SessionFactory sessionFactory) {

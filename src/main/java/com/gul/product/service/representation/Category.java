78b9a852c29c0c0bb4b48eb51fd228c246196474
Usman <azhar.rao@gmail.com>
2015-12-31 16:34:01 -0800

    updated category with imagepath

--- src/main/java/com/gul/product/service/representation/Category.java
+++ src/main/java/com/gul/product/service/representation/Category.java
@@ -3,7 +3,6 @@ package com.gul.product.service.representation;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,15 +10,12 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gul.product.service.audit.TimeStamped;
 

66dea688bf304e5b0444fb20609a8b01d2f354ea
Usman <azhar.rao@gmail.com>
2016-09-12 17:35:08 +0500

    add shipping documentation

--- src/main/java/com/gul/product/service/resources/ShippingResource.java
+++ src/main/java/com/gul/product/service/resources/ShippingResource.java
@@ -1,9 +1,7 @@
 package com.gul.product.service.resources;
 
 import io.dropwizard.hibernate.UnitOfWork;
-
 import java.util.Collection;
-
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -13,13 +11,15 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.hibernate.validator.constraints.NotEmpty;
-
 import com.codahale.metrics.annotation.Timed;
 import com.gul.product.service.persistance.ShippingDao;
 import com.gul.product.service.representation.ShipsTo;
+import com.wordnik.swagger.annotations.ApiOperation;
 
+/**
+ * add all where the product could be shipped to.
+ **/
 @Path("/shipping")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
@@ -34,6 +34,10 @@ public class ShippingResource {
 	@POST
 	@UnitOfWork
 	@Timed
+	@ApiOperation(
+            value = "Add where all the product can be shipped",
+            notes = "Where all the product could be shipped",
+            response = ShipsTo.class)	
 	public Response add(@Valid ShipsTo shipping) {
 		ShipsTo shippingInfo = shippingDao.create(shipping);
 		Collection<ShipsTo> shippingTo = shipping.getShippingTo();
@@ -48,7 +52,11 @@ public class ShippingResource {
 	@GET
 	@UnitOfWork
 	@Path("/{id}")
-	public Response getProduct(@PathParam("id") @NotEmpty Long id) {
+	@ApiOperation(
+            value = "Get shipping information.",
+            notes = "Get information where the product is being shipped to.",
+            response = ShipsTo.class)	
+	public Response getShipping(@PathParam("id") @NotEmpty Long id) {
 		ShipsTo shipping = shippingDao.findById(id);
 		return Response.status(Response.Status.OK).entity(shipping).build();
 	}
